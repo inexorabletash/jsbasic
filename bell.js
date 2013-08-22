98a8ba581a4ab2f2d42a33d1b3b11485ae2c06d2
Joshua Bell <inexorabletash@gmail.com>
2013-08-21 23:07:46 -0700

    Remove copyright/license from individual JS files

--- bell.js
+++ bell.js
@@ -1,21 +1,6 @@
 //
-// Applesoft BASIC in Javascript
 // Bell - play an audio file for the CHR$(7) "BEL" beep
 //
-
-// Copyright (C) 2009-2011 Joshua Bell
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 var Bell;
 if (typeof Bell !== 'function') {
